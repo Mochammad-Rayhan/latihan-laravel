68103df5cb5acbf809c9cd058db2338426fab46a
Mochammad Rayhan <mochammadrayhan05@gmail.com>
2025-12-15 15:55:09 +0700

    First Commit

---
+++ resources/views/beranda.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beranda</title>
+    <style>
+        nav {
+            display: flex;
+            justify-content: space-around;
+            padding: 25px 20px;
+            background-color: lightblue;
+            top: 0;
+            left: 0;
+            right: 0;
+            position: fixed;
+            z-index: 9999;
+            align-items: center;
+            width: 100%;
+        }
+
+        nav .judul {
+            font-size: 30px;
+            color: black;
+            text-decoration: none;
+        }
+
+        nav .navbar a {
+            margin-right: 13px;
+            font-size: 18px;
+            text-decoration: none;
+            color: black;
+        }
+
+        nav .navbar a:hover {
+            color: #fff;
+        }
+    </style>
+</head>
+<body>
+    <nav>
+        <a class="judul" href="">Mochammad Rayhan</a>
+
+        <div class="navbar">
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Portofolio</a>
+            <a href="">Contact Us</a>
+        </div>
+    </nav>
+</body>
+</html>
